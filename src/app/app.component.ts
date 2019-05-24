0c9bf7ee466973aaf87e24e403e57754c61f6a74
binarygeotech <geodavisitsolutions@live.com>
2019-05-24 11:12:25 +0100

    4 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import * as moment from 'moment';
+import { DaterangepickerComponent, DaterangepickerDirective } from 'ngx-daterangepicker-material';
+import { DRPickerDirective } from './directives/drpicker.directive';
+
+// import { pickerDirective } from './directives/drpicker.directive';
 
 @Component({
   selector: 'my-app',
@@ -7,4 +12,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular';
+
+  selected;
+
+  // @ViewChild(DaterangepickerDirective) pickerDirective: DaterangepickerDirective;
+  @ViewChild(DRPickerDirective) pickerDirective: DRPickerDirective;
+
+  inlineDate: any;
+  picker: DaterangepickerComponent;
+
+  
+  ngOnInit() {
+    this.picker = this.pickerDirective.picker;
+
+    // console.log(this.pickerDirective);
+  }
+
+  open(e) {
+    // console.log(this.pickerDirective);
+    // this.pickerDirective.open(e);
+    this.picker.show(e);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { NgxDaterangepickerMd } from 'ngx-daterangepicker-material';
+
+import { DRPickerDirective } from './directives/drpicker.directive';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [ BrowserModule, FormsModule, NgxDaterangepickerMd.forRoot(), ],
+  declarations: [ AppComponent, HelloComponent, DRPickerDirective ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
